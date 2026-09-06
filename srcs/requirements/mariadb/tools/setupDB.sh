10fe050bef06eac482896e3a2fbe3cd33cc4f98f
seg-fault0 <walidhemokok@gmail.com>
2026-09-06 08:09:52 -0400

    remove passwords from env file and add secret dir

--- srcs/requirements/mariadb/tools/setupDB.sh
+++ srcs/requirements/mariadb/tools/setupDB.sh
@@ -3,14 +3,17 @@
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
+AUP=$(cat /run/secrets/db_alter_user_pw)
+UP=$(cat /run/secrets/db_wp_user_pw)
+
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
     mysqld --user=mysql --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${AUP}';
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${UP}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
--- srcs/requirements/wordpress/tools/setupWP.sh
+++ srcs/requirements/wordpress/tools/setupWP.sh
@@ -11,29 +11,36 @@ if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
 
     cp wp-config-sample.php wp-config.php
 
+	PW=$(cat /run/secrets/db_wp_user_pw)
     sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
     sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
+    sed -i "s/password_here/$PW/g" wp-config.php
     sed -i "s/localhost/mariadb:3306/g" wp-config.php
 
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
 
+	while ! nc -z mariadb 3306; do
+        sleep 2
+    done
+
+	ADMIN_PW=$(cat /run/secrets/wp_admin_pw)
     wp core install \
         --url="https://wimam.42.fr" \
         --title="My WordPress Site" \
-        --admin_user="sitechief" \
-        --admin_password="SecureBossPassword123!" \
-        --admin_email="chief@yourdomain.com" \
+        --admin_user="$WP_SUPER_USER" \
+        --admin_password="$ADMIN_PW" \
+        --admin_email="$SUPER_USER_EMAIL" \
         --allow-root \
         --path='/var/www/html/wordpress'
 
+    USER_PW=$(cat /run/secrets/wp_user_pw)
     wp user create \
-        authoruser \
-        author@yourdomain.com \
+        "$WP_USER" \
+        "$USER_EMAIL" \
         --role=author \
-        --user_pass="SecureUserPassword123!" \
+        --user_pass="$USER_PW" \
         --allow-root \
         --path='/var/www/html/wordpress'
 fi
